0d475eba30976d38c468d26b1f5d288c1cff110e
JDisselhorst <JDisselhorst@users.noreply.github.com>
2016-09-19 17:05:11 +0200

    WSIC

--- JDisselhorst/DICOM/getPrivateFcn.m
+++
@@ -1,5 +0,0 @@
-function privateFcn = getPrivateFcn(privateDir,fcnName)
-  oldDir = cd(privateDir);         %# Change to the private directory
-  privateFcn = str2func(fcnName);  %# Get a function handle
-  cd(oldDir);                      %# Change back to the original directory
-end
--- JDisselhorst/PET/framing2MidTimes.m
+++
@@ -1,9 +0,0 @@
-%% framing2MidTimes: Convert framing to mid times from a dynamic pet 
-%
-% J.A. Disselhorst 2011. Radboud University Nijmegen Medical Centre
-% Input: Array of frame lengths,
-% Output: Array of frameMidTimes
-
-function frameMidTimes = framing2MidTimes(framing)
-    frameMidTimes = framing/2+cumsum([0 framing(1:end-1)]);  % Then at least the first element is correct.
-end
